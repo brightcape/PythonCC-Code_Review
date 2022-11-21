2dcef13b306fd55bd02ff14f4e50e5110cfea008
d.salome@brightcape.nl <d.salome@brightcape.nl>
2022-11-21 16:14:32 +0100

    First round of updates during CC meeting 2022-11-21

--- main_script_file.py
+++ main_script_file.py
@@ -3,38 +3,47 @@ from bs4 import BeautifulSoup
 import csv
 import os.path
 
-def GenerateHtml(base_url,payload):
 
-	r = requests.get(base_url, params=payload)
-	print ("Your'r on" ,r.url)
-	html_doc= (r.content)
-	return html_doc	
+def generate_html(url: str, payload=None) -> bytes:
+	"""
 
-def parsing_and_scrape_data(raw_data):
-		
-		
-		soup = BeautifulSoup(raw_data,'html.parser')
+	:param base_url:
+	:param payload: Data sent in the request
+	:return:
+	"""
+	response = requests.get(url, params=payload)
+	print("You're on", response.url)
+	html_doc = response.content
+	return html_doc
+
+
+def parse_data(html_doc: bytes) -> BeautifulSoup:
+	return BeautifulSoup(html_doc, 'html.parser')
+
+
+def scrape_data(soup):
+
 		column_data = soup.find_all({'div': 'column'})
-
-		for items in column_data:
-			for a in items.find_all('a'):
-				if 'onze-bieren' in a.get('href'):
-					product_details={}
-					ahref= a.get('href')
-					title = (a.text)
-					link=(ahref)
-					print (link)
-					product_details["Title"]=title
-					product_details["Links"]=link
-					write_on_csv(product_details)
+		for items in column_data:  # TODO Investigate if we can get rid of the for-loop
+			# Loop over all hyperlink sections
+			tbd = items.find_all('a')
+			for a in tbd:
+				link = a.get('href')
+				if 'onze-bieren' in link:
+					print(link)
+					product_details = {
+						"Title": a.text,
+						"Links": link
+					}
+					write_on_csv(product_details)  # TODO write csv at once after loading all data
 		
 		
 
 
-def write_on_csv(data):
+def write_on_csv(data, file_name='googleresults.csv'):
 
-	file_exists = os.path.isfile('googleresults.csv')
-	with open ('googleresults.csv', 'a',newline='') as csvfile:
+	file_exists = os.path.isfile(file_name)
+	with open (file_name, 'a',newline='') as csvfile:
 		fieldnames = ['Title', 'Links']
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 		
@@ -44,4 +53,9 @@ def write_on_csv(data):
 		writer.writerow(data)
 		print ('wrote on to the file')
 
-# https://100watt.nl/brouwerij/onze-bieren/
+
+if __name__ == '__main__':
+	url = 'https://100watt.nl/brouwerij/onze-bieren/'
+	raw_data = generate_html(url)
+	parsed_data = parse_data(raw_data)
+	scrape_data(parsed_data)
